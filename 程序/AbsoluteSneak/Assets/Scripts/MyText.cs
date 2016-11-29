65d0b35b927bfb75648e511f2fbfc59b561d9be6
Laichunjie <laichunjie111@163.com>
2016-11-29 15:48:15 +0800

    添加MyText脚本

---
+++ 程序/AbsoluteSneak/Assets/Scripts/MyText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MyText : MonoBehaviour {
+    public Text Mytext;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Mytext.text = Time.deltaTime.ToString();
+	}
+}
